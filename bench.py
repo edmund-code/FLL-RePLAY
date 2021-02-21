9e1a1df0c18e0301f7285b9b1b35e47e2f442574
Ching-Huei Tsou <tsou@MacBook-Pro.local>
2021-02-21 00:39:31 -0500

    code cleanup

--- bench.py
+++ bench.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, GyroSensor
-from pybricks.parameters import Port
-from pybricks.parameters import Direction
-from pybricks.robotics import DriveBase
+
+import fll_briarcliff6pack
+from pybricks.parameters import Stop
 import sys
 import time
 
-# Initialize the EV3 Brick.
-ev3 = EV3Brick()
-
-left_motor = Motor(Port.A)
-right_motor = Motor(Port.B)
-small_motor_left = Motor(Port.C, positive_direction=Direction.CLOCKWISE, gears=None)
-
-gyro_sensor = GyroSensor(Port.S3, Direction.CLOCKWISE)
-
-
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=93.5, axle_track=120)
+my_robot = fll_briarcliff6pack.Bri6Pack()
+drive_base = my_robot.drive_base
 
-robot.settings(straight_speed=100, turn_rate=50)
-robot.straight(-350) 
-robot.turn(11)
-robot.straight(-80)
-robot.straight(20)
-robot.turn(-15)
-robot.stop()
-robot.settings(straight_speed=500)
-robot.straight(-100)
-robot.straight(500)
+drive_base.settings(straight_speed=100, turn_rate=50)
+drive_base.straight(350) 
+drive_base.turn(11)
+drive_base.straight(80)
+drive_base.straight(-20)
+drive_base.turn(-15)
+drive_base.stop()
+drive_base.settings(straight_speed=500)
+drive_base.straight(100)
+drive_base.straight(-500)
